e202902435bfdadddecb7fdc8f098c62db8062dc
jiaxiang <jiaxiang002@e.ntu.edu.sg>
2024-01-05 14:10:26 +0800

    remove unused reference codebase

--- strategies/delta-hedge/src/delta_hedge/Derivative.py
+++
@@ -1,7 +0,0 @@
-class Derivative:
-    def __init__(self, name, payoff_fun, bid, ask, info):
-        self.name = name
-        self.payoff_fun = payoff_fun
-        self.deribit_info = info
-        self.bid = bid
-        self.ask = ask
