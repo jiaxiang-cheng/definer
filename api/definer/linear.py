d3be13e7811f426786ca1baf0c8279001894c2ec
jiaxiang <jiaxiang002@e.ntu.edu.sg>
2024-01-14 22:29:02 +0800

    add linear regression model can be constrained with non-negativity

--- api/definer/linear.py
+++ api/definer/linear.py
@@ -10,3 +10,26 @@ class LinearRegressionModel(nn.Module):
 
     def forward(self, x):
         return self.linear(x)
+
+
+# class LinearPositiveRegressionModel(nn.Module):
+#     def __init__(self, input_size):
+#         super(LinearPositiveRegressionModel, self).__init__()
+#         self.weight = nn.Parameter(torch.Tensor(1, input_size))
+#         # self.relu = nn.ReLU()
+#
+#     def forward(self, x):
+#         return nn.functional.linear(x, self.weight)
+
+
+class LinearPositiveRegressionModel(nn.Module):
+    def __init__(self, input_size):
+        super(LinearPositiveRegressionModel, self).__init__()
+        self.log_weight = nn.Parameter(torch.Tensor(1, input_size))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.log_weight)
+
+    def forward(self, x):
+        return nn.functional.linear(x, self.log_weight.exp())
