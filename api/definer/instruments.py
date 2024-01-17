d93c4041b5ac58566ac3a432e44a6f6f5fe39b3d
jiaxiang <jiaxiang002@e.ntu.edu.sg>
2024-01-17 12:59:54 +0800

    pricing options with black-scholes model

--- api/definer/instruments.py
+++ api/definer/instruments.py
@@ -1,3 +1,6 @@
+import math
+import numpy as np
+from scipy.stats import norm
 import requests
 
 
@@ -20,3 +23,20 @@ def retrieve_deribit_options(currency, expired=False):
     ).json()["result"]
 
     return instruments
+
+
+def black_scholes(df_options, initial_price, r, sigma):
+    df_options['pricing'] = 0
+    df_options['d1'] = (np.log(initial_price / df_options.strike_price.astype('float')) + (r + 0.5 * sigma ** 2) *
+                        df_options['T']) / (sigma * np.sqrt(df_options['T']))
+    df_options['d2'] = (np.log(initial_price / df_options.strike_price.astype('float')) + (r - 0.5 * sigma ** 2) *
+                        df_options['T']) / (sigma * np.sqrt(df_options['T']))
+    df_options.loc[df_options['option_type'] == 'C', 'pricing'] = (
+            (initial_price * norm.cdf(df_options['d1']) -
+             df_options.strike_price.astype('float') * np.exp(-r * df_options['T']) * norm.cdf(df_options['d2']))
+            / initial_price)
+    df_options.loc[df_options['option_type'] == 'P', 'pricing'] = (
+            (df_options.strike_price.astype('float') * np.exp(-r * df_options['T']) * norm.cdf(-df_options['d2']) -
+             initial_price * norm.cdf(-df_options['d1']))
+            / initial_price)
+    return df_options
